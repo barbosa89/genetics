a1ae302c6c97b64a478193ad8808bdcabfbce3a6
Jairo Harlem Villamarin Anacona <jairo.villamarinanacona@evertecinc.com>
2020-06-16 11:36:29 -0500

    Add the  parameter to 'create' method of repository contract.

--- app/Contracts/Repositories/Repository.php
+++ app/Contracts/Repositories/Repository.php
@@ -2,7 +2,7 @@
 
 namespace App\Contracts\Repositories;
 interface Repository {
-    public function create();
+    public function create(array $data);
     public function find(int $id);
     public function update(array $data, $id);
     public function delete(int $id);
