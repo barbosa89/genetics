d6f577665942ae339e6ecbada06a10d84bb9dbda
Barbosa <omar.barbosa@evertecinc.com>
2020-06-16 10:09:22 -0500

    Repository Starndard methods

--- app/Contracts/Repositories/FooRepository.php
+++ app/Contracts/Repositories/FooRepository.php
@@ -2,5 +2,8 @@
 
 namespace App\Contracts\Repositories;
 interface FooRepository {
-
+    public function create();
+    public function find(int $id);
+    public function update(array $data, $id);
+    public function delete(int $id);
 }
