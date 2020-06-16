1ea57777484305880fae6e518f2ff0b285f7e8e3
Barbosa <omar.barbosa@evertecinc.com>
2020-06-16 09:41:47 -0500

    Init

---
+++ app/Contracts/Common/Standard.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Common interface
+ *
+ * @package Genetics
+ */
+interface Standard
+{
+    /**
+     * Get model from database
+     *
+     * @return Model
+     */
+    public function get();
+}
